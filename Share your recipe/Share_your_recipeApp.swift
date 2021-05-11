a45ce19fb4560c71c97b303ce704f91a2e289a31
Ian Donker <iandonker7@gmail.com>
2021-05-11 11:46:56 +0200

    Initial Commit

---
+++ Share your recipe/Share_your_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_your_recipeApp.swift
+//  Share your recipe
+//
+//  Created by Ian Donker on 11/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Share_your_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
